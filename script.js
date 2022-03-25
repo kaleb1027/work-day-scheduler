7f5202f5050ad622bc5bad4e2e35f5eba0de2a60
Kaleb Sherrill <kaleb1027@gmail.com>
2022-03-24 21:31:27 -0400

    Setup save button local storage using jquery

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 currentDay = document.getElementById("currentDay");
-
 function refreshTime(){
     var rightNow = moment().format("MMMM Do, HH:mm:ss");
-    console.log(rightNow);
     currentDay.textContent = rightNow;
 }
-
 setInterval(refreshTime,1000);
+
+var saveButton = $(".saveBtn");
+console.log(saveButton);
+
+saveButton.on("click", function(){
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+
+    localStorage.setItem(time, text);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
