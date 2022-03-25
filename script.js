ba67c51a82eae4431738e7c9c55bf7c914b4fc3e
Kaleb Sherrill <kaleb1027@gmail.com>
2022-03-24 20:10:51 -0400

    Added updated time to header

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+currentDay = document.getElementById("currentDay");
 
+function refreshTime(){
+    var rightNow = moment().format("MMMM Do, HH:mm:ss");
+    console.log(rightNow);
+    currentDay.textContent = rightNow;
+}
+
+setInterval(refreshTime,1000);
