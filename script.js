c3a5292844afba70baabb7790220e5c9f41f6a27
Kaleb Sherrill <kaleb1027@gmail.com>
2022-03-24 21:41:04 -0400

    Local storage using jquery complete

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+//sets time
 currentDay = document.getElementById("currentDay");
 function refreshTime(){
     var rightNow = moment().format("MMMM Do, HH:mm:ss");
@@ -5,17 +9,37 @@ function refreshTime(){
 }
 setInterval(refreshTime,1000);
 
+
+
+//color codes past,present,and future
+
+
+//saves local storage
 var saveButton = $(".saveBtn");
 console.log(saveButton);
 
 saveButton.on("click", function(){
-    console.log(this);
     var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
-
-    localStorage.setItem(time, text);
+    var blockID = $(this).parent().attr("id");
+    localStorage.setItem(blockID, text);
 })
+
+$("#hour9 .description").text(localStorage.getItem("hour9"));
+$("#hour10 .description").text(localStorage.getItem("hour10"));
+$("#hour11 .description").text(localStorage.getItem("hour11"));
+$("#hour12 .description").text(localStorage.getItem("hour12"));
+$("#hour13 .description").text(localStorage.getItem("hour13"));
+$("#hour14 .description").text(localStorage.getItem("hour14"));
+$("#hour15 .description").text(localStorage.getItem("hour15"));
+$("#hour16 .description").text(localStorage.getItem("hour16"));
+$("#hour17 .description").text(localStorage.getItem("hour17"));
+
+
+
+
+
+
+
 
 
 
